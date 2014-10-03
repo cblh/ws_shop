4eb0ac5ce310aaada071a96d84c9882b9393d0ce
baolin <chenbeiliuhuo@gamil.com>
2014-10-03 17:18:44 +0800

    cart ajax init

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,95 @@
+class CartsController < ApplicationController
+  before_action :set_cart, only: [:show, :edit, :update, :destroy]
+
+  # GET /carts
+  # GET /carts.json
+  def index
+    @carts = Cart.all
+  end
+
+  # GET /carts/1
+  # GET /carts/1.json
+  def show
+    begin
+      @cart = Cart.find params[:id]
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: "不可用的购物车"
+    else
+      respond_to do |format|
+        format.html # show.html.haml
+        format.json { render json: @cart }
+      end
+    end
+  end
+
+  # GET /carts/new
+  def new
+    @cart = Cart.new
+  end
+
+  # GET /carts/1/edit
+  def edit
+  end
+
+  # POST /carts
+  # POST /carts.json
+  def create
+    @cart = current_cart
+    product = product.find(params[:product_id])
+    @line_item = @cart.add_product(product.id)
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to @line_item.cart, notice: 'Cart was successfully created.' }
+        format.json { render :show, status: :created, location: @line_item }
+      else
+        format.html { render :new }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /carts/1
+  # PATCH/PUT /carts/1.json
+  def update
+    respond_to do |format|
+      if @cart.update(cart_params)
+        format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
+        format.json { render :show, status: :ok, location: @cart }
+      else
+        format.html { render :edit }
+        format.json { render json: @cart.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /carts/1
+  # DELETE /carts/1.json
+  def destroy
+    @cart = current_cart
+    @cart.destroy
+    session[:cart_id] = nil
+
+    respond_to do |format|
+      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cart
+      begin
+        @cart = Cart.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        logger.error "Attempt to access invalid cart #{params[:id]}"
+        redirect_to store_path, notice: 'Invalid cart'
+      end
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def cart_params
+      params[:cart]
+    end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -8,11 +8,12 @@ class Product < ActiveRecord::Base
   private
     def ensure_not_referenced_by_any_line_item
       if line_items.empty?
-        return true
+        true
       else
-        error.add(:base, 'Line Items present')
-        return false
+        # error.add(:base, 'Line Items present')
+        false
       end
+      true
     end
 
 
@@ -20,7 +21,7 @@ class Product < ActiveRecord::Base
 
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0.01 }
-	validates :title, uniqueness: true, length: { minimum: 10, maximum: 50 }
+	validates :title, uniqueness: true, length: { minimum: 1, maximum: 50 }
 	validates :image_url, format: {
 		with: VALIE_IMAGE_URL_REGEX,
 		message: 'must be a URL for GIF, JPG or PNG image.'
