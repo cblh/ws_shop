80490ef96b1c0d1610ba4e33e75a457959f51de4
baolin <chenbeiliuhuo@gmail.com>
2014-10-06 21:56:50 +0800

    modify session_store config and fix bug

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-WsShop::Application.application.config.session_store :cookie_store, key: '_ws_shop_session'
+WsShop::Application.config.session_store :cookie_store, key: '_ws_shop_session'
