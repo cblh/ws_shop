231f2dfd9496fbc630ae0af54d262cc590b02bfc
baolin <chenbeiliuhuo@gmail.com>
2014-10-07 16:06:32 +0800

    move and modify product_img_tag helper, and modify product view

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,12 +1,2 @@
 module ProductsHelper
-  def product_img_tag(product, options = {})
-    options[:style] ||= :small
-    style = case options[:style].to_s
-    when 'small' then '30x30'
-    when 'normal' then '100x100'
-    when 'large' then '240x240'
-    else options[:style].to_s
-    end
-    link_to image_tag(product.image.url(style)), product
-  end
 end
